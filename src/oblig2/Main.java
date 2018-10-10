2ad1a3de61f928bd65ad3fe2a2072b5a134616fe
JakobR <32777603+jakobBR@users.noreply.github.com>
2018-10-10 21:25:57 +0200

    l

---
+++ src/oblig2/Main.java
@@ -0,0 +1,17 @@
+package oblig2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] navn = {"Lars","Anders","Bodil","Kari","Per","Berit"};
+        Liste<String> liste = new DobbeltLenketListe<>(navn);
+
+        liste.forEach(s -> System.out.print(s + " "));
+        System.out.println();
+        for (String s : liste) System.out.print(s + " ");
+
+        // Utskrift:
+        // Lars Anders Bodil Kari Per Berit
+        // Lars Anders Bodil Kari Per Berit
+    }
+}
